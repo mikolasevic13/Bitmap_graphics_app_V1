e6fe170f5e9d811c4e695b99abe0d63bdd9935af
mikolasevic13 <josip.mikolasevic@gmail.com>
2026-05-09 11:45:13 +0200

    Add files via upload

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Bitmap_slika_meta1
+﻿namespace Bitmap_slika_meta1
 {
     partial class MainForm
     {
@@ -29,218 +28,42 @@ namespace Bitmap_slika_meta1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.buttonSAVE = new System.Windows.Forms.Button();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.DUPLOsave = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button7 = new System.Windows.Forms.Button();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
-            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
-            this.toolTip4 = new System.Windows.Forms.ToolTip(this.components);
-            this.toolTip5 = new System.Windows.Forms.ToolTip(this.components);
-            this.randimage_btn = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.DECODE_A = new System.Windows.Forms.Button();
+            this.btnPaint = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(17, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(590, 460);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.toolTip2.SetToolTip(this.pictureBox1, "To load the image into picturebox A, double click and follow the instructions. \r\n" +
-        "");
             this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
             // 
-            // buttonSAVE
+            // btnPaint
             // 
-            this.buttonSAVE.Location = new System.Drawing.Point(959, 520);
-            this.buttonSAVE.Name = "buttonSAVE";
-            this.buttonSAVE.Size = new System.Drawing.Size(291, 23);
-            this.buttonSAVE.TabIndex = 2;
-            this.buttonSAVE.Text = "SAVE B";
-            this.toolTip1.SetToolTip(this.buttonSAVE, "Save image B to file.\r\n");
-            this.buttonSAVE.UseVisualStyleBackColor = true;
-            this.buttonSAVE.Click += new System.EventHandler(this.buttonSAVE_Click);
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Location = new System.Drawing.Point(660, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(590, 460);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 4;
-            this.pictureBox2.TabStop = false;
-            this.toolTip3.SetToolTip(this.pictureBox2, "To load the image into picturebox B, double click and follow the instructions.\r\n");
-            this.pictureBox2.DoubleClick += new System.EventHandler(this.pictureBox2_DoubleClick);
-            // 
-            // DUPLOsave
-            // 
-            this.DUPLOsave.Location = new System.Drawing.Point(959, 549);
-            this.DUPLOsave.Name = "DUPLOsave";
-            this.DUPLOsave.Size = new System.Drawing.Size(291, 23);
-            this.DUPLOsave.TabIndex = 12;
-            this.DUPLOsave.Text = "APPEND A & B images HORIZONTALLY";
-            this.toolTip4.SetToolTip(this.DUPLOsave, "Appends images A and B horizontally and saves them to local file");
-            this.DUPLOsave.UseVisualStyleBackColor = true;
-            this.DUPLOsave.Click += new System.EventHandler(this.DUPLOsave_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(280, 475);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 42);
-            this.label2.TabIndex = 14;
-            this.label2.Text = "A";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(935, 475);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 42);
-            this.label3.TabIndex = 15;
-            this.label3.Text = "B";
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(959, 578);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(291, 23);
-            this.button7.TabIndex = 16;
-            this.button7.Text = "APPEND A & B images VERTICALLY";
-            this.toolTip5.SetToolTip(this.button7, "Appends images A and B vertically and saves them to local file");
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // toolTip1
-            // 
-            this.toolTip1.IsBalloon = true;
-            // 
-            // toolTip2
-            // 
-            this.toolTip2.IsBalloon = true;
-            this.toolTip2.ToolTipTitle = "LOAD IMAGE A";
-            // 
-            // toolTip3
-            // 
-            this.toolTip3.IsBalloon = true;
-            this.toolTip3.ToolTipTitle = "LOAD IMAGE B";
-            // 
-            // toolTip4
-            // 
-            this.toolTip4.ToolTipTitle = "APPEND A & B horizontally";
-            // 
-            // toolTip5
-            // 
-            this.toolTip5.ToolTipTitle = "APPEND A & B vertically";
-            // 
-            // randimage_btn
-            // 
-            this.randimage_btn.Location = new System.Drawing.Point(17, 520);
-            this.randimage_btn.Name = "randimage_btn";
-            this.randimage_btn.Size = new System.Drawing.Size(291, 23);
-            this.randimage_btn.TabIndex = 1;
-            this.randimage_btn.Text = "Random image - INPUT IMAGE";
-            this.randimage_btn.UseVisualStyleBackColor = true;
-            this.randimage_btn.Click += new System.EventHandler(this.btnPaint_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(17, 578);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(307, 71);
-            this.textBox1.TabIndex = 18;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 554);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(149, 13);
-            this.label4.TabIndex = 19;
-            this.label4.Text = "ORIGINAL USER MESSAGE:";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(330, 578);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 23);
-            this.button1.TabIndex = 20;
-            this.button1.Text = "CODE MESSAGE A to B";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(482, 554);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 13);
-            this.label1.TabIndex = 21;
-            this.label1.Text = "DECODED USER MESSAGE:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(485, 578);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(307, 71);
-            this.textBox2.TabIndex = 22;
-            // 
-            // DECODE_A
-            // 
-            this.DECODE_A.Location = new System.Drawing.Point(799, 578);
-            this.DECODE_A.Name = "DECODE_A";
-            this.DECODE_A.Size = new System.Drawing.Size(133, 23);
-            this.DECODE_A.TabIndex = 24;
-            this.DECODE_A.Text = "DECODE image A";
-            this.DECODE_A.UseVisualStyleBackColor = true;
-            this.DECODE_A.Click += new System.EventHandler(this.DECODE_A_Click);
+            this.btnPaint.Location = new System.Drawing.Point(13, 479);
+            this.btnPaint.Name = "btnPaint";
+            this.btnPaint.Size = new System.Drawing.Size(589, 23);
+            this.btnPaint.TabIndex = 1;
+            this.btnPaint.Text = "Paint Bullseye";
+            this.btnPaint.UseVisualStyleBackColor = true;
+            this.btnPaint.Click += new System.EventHandler(this.btnPaint_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1263, 686);
-            this.Controls.Add(this.DECODE_A);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.DUPLOsave);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.buttonSAVE);
-            this.Controls.Add(this.randimage_btn);
+            this.ClientSize = new System.Drawing.Size(614, 516);
+            this.Controls.Add(this.btnPaint);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "MainForm";
-            this.Text = "BITstring";
+            this.Text = "Bitmap Graphics";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -249,24 +72,7 @@ namespace Bitmap_slika_meta1
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button buttonSAVE;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Button DUPLOsave;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.ToolTip toolTip2;
-        private System.Windows.Forms.ToolTip toolTip3;
-        private System.Windows.Forms.ToolTip toolTip4;
-        private System.Windows.Forms.ToolTip toolTip5;
-        private System.Windows.Forms.Button randimage_btn;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button DECODE_A;
+        private System.Windows.Forms.Button btnPaint;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -2,22 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Deployment.Internal;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Schema;
-using static System.Net.Mime.MediaTypeNames;
-
-
-using Image = System.Drawing.Image;
 
 namespace Bitmap_slika_meta1
 {
@@ -27,319 +16,36 @@ namespace Bitmap_slika_meta1
         {
             InitializeComponent();
         }
-        //random input image klika na gumb Random image - input image
-
-        public string abeceda = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,!:=/$%;0123456789ŠĐČĆŽšđčćž?()#$%&?+-*_-<>";
 
         private void btnPaint_Click(object sender, EventArgs e)
         {
+            Bitmap drawingSurface = new Bitmap(1000, 1000);
+            Graphics GFX = Graphics.FromImage(drawingSurface);
 
-            Bitmap bimp = new Bitmap(800, 800);
-            for (int i = 0; i < 800; i++)
-                for (int j = 0; j < 800; j++)
-                    bimp.SetPixel(i, j, Color.White);
-            this.pictureBox1.Image = bimp;
+            //dijagonalni krugovi
+            //GFX.FillEllipse(Brushes.Red, 0, 0, 1000, 1000);
+            //GFX.FillEllipse(Brushes.White, 0, 0, 800, 800);
+            //GFX.FillEllipse(Brushes.Red, 0, 0, 600, 600);
+            //GFX.FillEllipse(Brushes.White, 0, 0, 400, 400);
+            //GFX.FillEllipse(Brushes.Red, 0, 0, 200, 200);
 
-
-            
-            for (int x = 0; x < bimp.Width; x++)
-                for (int y = 0; y < bimp.Height; y++)
-                {
-                    Random r = new Random(Guid.NewGuid().GetHashCode());
-
-                    int rand_red = r.Next(0, 5000);
-                    rand_red = rand_red % 255;
-                    int rand_green = r.Next(0, 5000);
-                    rand_green = rand_green % 255;
-                    int rand_blue = r.Next(0, 5000);
-                    rand_blue = rand_blue % 255;
-                    bimp.SetPixel(x, y, Color.FromArgb(255, rand_red, rand_green, rand_blue));
-                }
-                    
-            bimp.Save("C:\\VS_slikeOUT\\rand_image.bmp");
-            this.pictureBox1.Image = bimp;
-
+            GFX.FillEllipse(Brushes.Red, 0, 0, 1000, 1000);
+            GFX.FillEllipse(Brushes.White, 100, 100, 800, 800);
+            GFX.FillEllipse(Brushes.Red, 200, 200, 600, 600);
+            GFX.FillEllipse(Brushes.White, 300, 300, 400, 400);
+            GFX.FillEllipse(Brushes.Red, 400, 400, 200, 200);
+            pictureBox1.Image = drawingSurface;
         }
-        
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-            Bitmap bimp = new Bitmap(800, 800);
-            bimp = (Bitmap)pictureBox1.Image;
-            string original = textBox1.Text;
-            string abeceda = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,!:=/$%;0123456789ŠĐČĆŽšđčćž?()#$%&?+-*_-<>";
-
-            Bitmap codedbit = new Bitmap(800, 800);
-
-            int i=0,j=0;
-            int br_znak=0;
-            int[] pomak= new int[800];
-            
-            for (i = 0; i < 800; i++)
-            {
-                br_znak = 0;
-                if (i < original.Length)
-                {
-                    char znak = original[i];
-                    for(j=0;j<abeceda.Length;j++)
-                    {
-                        if (znak == abeceda[j])
-                            br_znak = j + 65;
-                    }
-                    pomak[i] = br_znak;
-                }
-                else
-                    pomak[i] = 0;
-
-            }
-            /*ispis u textbox proba
-            for (i = 0; i < 800; i++)
-            {
-                textBox2.Text += pomak[i]+" ";
-
-            }
-            */
-
-            for (int y = 0; y < 800; y++)
-            {
-                    for (int x = 0; x < 800; x++)
-                    {
-                        int r = bimp.GetPixel(x, y).R;
-                        int g = bimp.GetPixel(x, y).G;
-                        int b = bimp.GetPixel(x, y).B;
-                    if ((x == pomak[y]) & (y< original.Length))
-                    {
-                        if(y%3==0)
-                        {
-                            if (r > 1)
-                                codedbit.SetPixel(x, y, Color.FromArgb(255, r - 1, g, b));
-                            else
-                                codedbit.SetPixel(x, y, Color.FromArgb(255, r + 1, g, b));
-                            if (r < 255)
-                                codedbit.SetPixel(x, y, Color.FromArgb(255, r + 1, g, b));
-                        }
-                            
-                        if (y % 3 == 1)
-                        {
-                            if (g > 1) 
-                                codedbit.SetPixel(x, y, Color.FromArgb(255, r , g-1, b));
-                            else
-                                codedbit.SetPixel(x, y, Color.FromArgb(255, r, g + 1, b));
-                            if (g < 255)
-                                codedbit.SetPixel(x, y, Color.FromArgb(255, r , g+1, b));
-                        }
-                        codedbit.SetPixel(x, y, Color.FromArgb(255, r, g-1, b));
-                        if (y % 3 == 2)
-                        {
-                            if (b > 1) 
-                                codedbit.SetPixel(x, y, Color.FromArgb(255, r , g, b-1));
-                            else
-                                codedbit.SetPixel(x, y, Color.FromArgb(255, r, g, b + 1));
-                            if (b < 255)
-                                codedbit.SetPixel(x, y, Color.FromArgb(255, r , g, b+1));
-                        }
-                        
-                    }
-                    else
-                        {
-                            codedbit.SetPixel(x, y, Color.FromArgb(255, r, g, b));
-                        }
-
-                    }
-                
-            }
-
-            codedbit.Save("C:\\VS_slikeOUT\\code_image.bmp");
-            this.pictureBox2.Image = codedbit;
-        }
-        //DEKODIRANJE IZ SLIKE
-        private void DECODE_A_Click(object sender, EventArgs e)
-        {
-
-            
-
-            //Bitmap bimp = new Bitmap(@"C:\\VS_slike\\outputH.bmp",true);
-            Bitmap bimp = (Bitmap)pictureBox1.Image;
-            string im1, im2;
-            string abeceda = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,!:=/$%;0123456789ŠĐČĆŽšđčćž?()#$%&?+-*_-<>";
-            char deznak=' ';
-            int i = 0, j = 0,x,y;
-            int br_znak;
-            int prazno=0;
-            int[] pomak = new int[800];
-            int kontrol = 0;
-            
-            //PRONAĐI I POKUPI RAZLIKE U INTENZITETIMA...
-            //VEKTOR DEKODIRAN POMAK[Y]
-            for ( y = 0; y < 800; y++)
-            {
-                kontrol = 0;
-                for ( x = 0; x < 800; x++)
-                {
-                    Color pix1 = bimp.GetPixel(x, y);
-                    Color pix2 = bimp.GetPixel(x+800, y);
-                    int r = (int)pix1.R;
-                    int g = (int)pix1.G;
-                    int b = (int)pix1.B;
-                    int rd = (int)pix2.R;
-                    int gd = (int)pix2.G;
-                    int bd = (int)pix2.B;
-                    
-                    if (pix1!=pix2)
-                    {
-                        pomak[y] = x;
-                        kontrol++;
-                        break;
-                    }
-                    else
-                    {
-                        pomak[y] = 0;
-                    }
-                }
-                
-
-            }
-            //DEKODIRATI IZ INT U CHAR ZNAK PO ZNAK POMAK[Y]
-            string dekod="";
-            for(i=0;i<800;i++)
-            {
-                if (pomak[i] != 0)
-                {
-                    br_znak = pomak[i];
-                    deznak = abeceda[br_znak-65];
-                }
-                else
-                    deznak = ' ';
-                textBox2.Text += deznak;
-            }
-        }
-        
 
         private void pictureBox1_DoubleClick(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Bitmaps|*.bmp|jpegs|*.jpg";
 
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            if( openFileDialog.ShowDialog()==DialogResult.OK )
             {
                 pictureBox1.ImageLocation = openFileDialog.FileName;
             }
-        }
-
-        private void pictureBox2_DoubleClick(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Bitmaps|*.bmp|jpegs|*.jpg";
-
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                pictureBox2.ImageLocation = openFileDialog.FileName;
-            }
-        }
-
-        private void buttonSAVE_Click(object sender, EventArgs e)
-        {
-            // postavljanje naziva datoteke koji sadrži datum i vrijeme kreiranja
-            string imgName = DateTime.Now.Day + "_" + DateTime.Now.Month + "_" + DateTime.Now.Year + "_h_" + DateTime.Now.Hour + "_" + DateTime.Now.Minute + "_" + DateTime.Now.Second + "." + ImageFormat.Jpeg;
-            SaveFileDialog svf = new SaveFileDialog();
-
-            svf.Filter = "Image Files (*.jpeg)|*.jpeg";
-            svf.DefaultExt = "jpeg";
-            svf.AddExtension = true;
-            if (svf.ShowDialog() == DialogResult.OK)
-            {
-                pictureBox2.Image.Save(svf.FileName);
-            }
-        }
-
-        //Spajanje snimljenih slika HORIZONTALNO
-        private void DUPLOsave_Click(object sender, EventArgs e)
-        {
-            pictureBox1.Image.Save("C:\\VS_slikeOUT\\a.bmp");
-            pictureBox2.Image.Save("C:\\VS_slikeOUT\\b.bmp");
-
-            Image img1 = Image.FromFile(("C:\\VS_slikeOUT\\a.bmp"), true);
-            Image img2 = Image.FromFile(("C:\\VS_slikeOUT\\b.bmp"), true);
-
-
-            List<Image> fileList = new List<Image>();
-            fileList.Add(img1);
-            fileList.Add(img2);
-
-            Bitmap bitmap1 = MergeImagesH(fileList);
-            bitmap1.Save(("C:\\VS_slikeOUT\\outputH.bmp"), ImageFormat.Bmp);
-        }
-
-        public static Bitmap MergeImagesH(List<Image> images)
-        {
-            int outputImageWidth = 0;
-            int outputImageHeight = 1;
-            foreach (Image image in images)
-            {
-                //HORIZONTALNO
-                outputImageWidth += image.Width;
-                if (image.Height > outputImageHeight)
-                {
-                    outputImageHeight = image.Height;
-                }
-                
-            }
-
-            Bitmap outputImage = new Bitmap(outputImageWidth, outputImageHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-
-            using (Graphics graphics = Graphics.FromImage(outputImage))
-            {
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                graphics.Clear(Color.White);
-                graphics.DrawImage(images[0], new Rectangle(new Point(), images[0].Size), new Rectangle(new Point(), images[0].Size), GraphicsUnit.Pixel);
-                graphics.DrawImage(images[1], new Rectangle(new Point(images[0].Width, 0 ), images[1].Size), new Rectangle(new Point(), images[1].Size), GraphicsUnit.Pixel);
-            }
-
-            return outputImage;
-        }
-
-        public static Bitmap MergeImagesV(List<Image> images)
-        {
-            int outputImageWidth = 0;
-            int outputImageHeight = 1;
-            foreach (Image image in images)
-            {
-                                //VERTIKALNO
-                outputImageHeight += image.Height;
-                if (image.Width > outputImageWidth)
-                {
-                    outputImageWidth = image.Width;
-                }
-            }
-
-            Bitmap outputImage = new Bitmap(outputImageWidth, outputImageHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-
-            using (Graphics graphics = Graphics.FromImage(outputImage))
-            {
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                graphics.Clear(Color.White);
-                graphics.DrawImage(images[0], new Rectangle(new Point(), images[0].Size), new Rectangle(new Point(), images[0].Size), GraphicsUnit.Pixel);
-                graphics.DrawImage(images[1], new Rectangle(new Point(0, images[0].Height), images[1].Size), new Rectangle(new Point(), images[1].Size), GraphicsUnit.Pixel);
-            }
-
-            return outputImage;
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            pictureBox1.Image.Save("C:\\VS_slike\\c.bmp");
-            pictureBox2.Image.Save("C:\\VS_slike\\d.bmp");
-
-            Image img1 = Image.FromFile(("C:\\VS_slike\\c.bmp"), true);
-            Image img2 = Image.FromFile(("C:\\VS_slike\\d.bmp"), true);
-
-
-            List<Image> fileList = new List<Image>();
-            fileList.Add(img1);
-            fileList.Add(img2);
-
-            Bitmap bitmap1 = MergeImagesV(fileList);
-            bitmap1.Save(("C:\\VS_slike\\outputV.bmp"), ImageFormat.Bmp);
         }
     }
 }
